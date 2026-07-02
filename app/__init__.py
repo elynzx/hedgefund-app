35be4656c7f05c8e25edaa20d80d3ef9d72e0520
Elynzx <rpascual06@outlook.com>
2026-07-02 10:39:12 -0500

    initialize Flask-Migrate in the application setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from config import Config
 from db import db
+from flask_migrate import Migrate
+from app.models import (user, category, bank_account, credit_card, transaction)
+
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    migrate.init_app(app, db)
 
     return app
