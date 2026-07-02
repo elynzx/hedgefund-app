adacfcf0a5280285b60254856516af07c556d1f8
Elynzx <rpascual06@outlook.com>
2026-07-01 21:23:36 -0500

    refactor Category model to use Enum for category_type and update check constraint

--- app/models/category.py
+++ app/models/category.py
@@ -1,18 +1,25 @@
 from db import db
 from sqlalchemy import Integer, String, CheckConstraint, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
+from enum import Enum
 
+class CategoryType(str, Enum):
+    INCOME = 'income'
+    FIXED_EXPENSE = 'fixed_expense'
+    VARIABLE_EXPENSE = 'variable_expense'
+    SYSTEM = 'system'
+    
 class Category(db.Model):
     __tablename__ = 'categories'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     category_name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
-    category_type: Mapped[str] = mapped_column(String(20), nullable=False)
+    category_type: Mapped[CategoryType] = mapped_column(String(20), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
 
     __table_args__ = (
         CheckConstraint(
-            "category_type IN ('income', 'fixed_expense', 'variable_expense', 'system')", 
+             f"category_type IN ('{CategoryType.INCOME}', '{CategoryType.FIXED_EXPENSE}', '{CategoryType.VARIABLE_EXPENSE}', '{CategoryType.SYSTEM}')", 
             name='chk_category_type'
         ),
     )
