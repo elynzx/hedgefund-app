96431ac73b95dfdc6fe513eb1a57e2314663c7f1
Elynzx <rpascual06@outlook.com>
2026-07-02 10:39:06 -0500

    fix typo in SQLALCHEMY_DATABASE_URI variable name in config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config():
     DEBUG=True
-    SQLALCHEMIST_DATABASE_URI= os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI= os.getenv('DATABASE_URI')
     SECRET_KEY= os.getenv('SECRET_KEY')
     JWT_ACCES_TOKEN_EXPIRES=timedelta(days=1)
     JWT_REFRESH_TOKEN_EXPPIRES=timedelta(days=30)
