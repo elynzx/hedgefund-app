6f98109bea71b7bf74a0daf686f631257bc00778
Elynzx <rpascual06@outlook.com>
2026-07-04 20:14:56 -0500

    initialize Bcrypt for password hashing in the application

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,17 @@ from flask import Flask
 from config import Config
 from db import db
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 from app.models import (user, category, bank_account, credit_card, transaction)
 
 migrate = Migrate()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
     migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
     return app
