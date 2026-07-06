a25bd4543e372138f917f50b9acd9bc475b34465
Elynzx <rpascual06@outlook.com>
2026-07-06 00:48:36 -0500

    refactor UserUpdateSchema to use union types and improve currency validation logic

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -3,18 +3,18 @@ from datetime import datetime
 from typing import Optional
 
 class UserUpdateSchema(BaseModel):
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-    currency: Optional[str] = Field(None, min_length=3, max_length=3)
-    monthly_income: Optional[float] = Field(None, ge=0.00)
+    first_name: str | None = None
+    last_name: str | None = None
+    currency: str | None = Field(None, min_length=3, max_length=3)
+    monthly_income: float | None = Field(None, ge=0.00)
     
     @field_validator('currency')
-    def validate_currency(cls, value: Optional[str]) -> Optional[str]:
+    def validate_currency(cls, value:str | None) -> str | None:
         if value is None:
             return value
 
-        value = value.upper().strip()
-        allowed_currencies = ["PEN", "USD", "EUR"]
+        value = value.strip().upper()
+        allowed_currencies = {"PEN", "USD", "EUR"}
         
         if value not in allowed_currencies:
             raise ValueError(f"Moneda no soportada. Permitidas: {', '.join(allowed_currencies)}")
