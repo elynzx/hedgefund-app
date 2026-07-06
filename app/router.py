6a6b6e1dfdb9733d6918b9e9fab6f1457f879342
Elynzx <rpascual06@outlook.com>
2026-07-06 12:05:16 -0500

    fix: correct Api initialization by removing app parameter

--- app/router.py
+++ app/router.py
@@ -2,7 +2,7 @@ from flask_restful import Api
 from app import app
 from app.resources.auth_resource import *
 
-api = Api(app, prefix='/api/v1')
+api = Api(prefix='/api/v1')
 
 api.add_resource(RegisterResource, '/auth/register')
 api.add_resource(LoginResource, '/auth/login')
