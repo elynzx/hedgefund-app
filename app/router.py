1ca7f87079085b9dc4db17a66fa9bb55177c85eb
Elynzx <rpascual06@outlook.com>
2026-07-06 17:13:26 -0500

    feat: add TransactionResource route for transaction management

--- app/router.py
+++ app/router.py
@@ -3,6 +3,7 @@ from app.resources.auth_resource import *
 from app.resources.user_resource import *
 from app.resources.bank_account_resource import *
 from app.resources.credit_card_resource import *
+from app.resources.transaction_resource import *
 
 api = Api(prefix='/api/v1')
 
@@ -17,3 +18,4 @@ api.add_resource(ManageBankAccountResource, '/accounts/<int:account_id>')
 api.add_resource(CreditCardResource, '/cards')
 api.add_resource(ManageCreditCardResource, '/cards/<int:card_id>')
 
+api.add_resource(TransactionResource, '/transactions')
